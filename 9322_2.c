65954ec54bbeacd8d0026261c750fa9706aa715a
윤현지 <hyoon@student.42seoul.kr>
2021-01-25 13:47:08 +0900

    update 23th week mine

--- 9322_2.c
+++ 9322_2.c
@@ -46,6 +46,46 @@ void		get_plain_text(char **secret, int *key, int m)
 	}
 	printf("\n");
 }
+//문자열을 복사하여 저장하는 함수
+char		*get_short_string(char *str, int start, int end)
+{
+	int		i;
+	char	*new;
+
+
+	if (!(new = (char *)malloc(sizeof(char) * (end - start + 1))))
+		return (NULL);
+	i = -1;
+	while (++i < end - start)
+		new[i] = str[start + i];
+	new[i] = 0;
+	return (new);
+}
+//스페이스를 기준으로 문자열을 나누어 이중배열로 저장하는 함수
+char		**get_string(char *str, int m)
+{
+	int		i;
+	int		start;
+	int		count;
+	char	**key;
+
+	if (!(key = (char **)malloc(sizeof(char *) * (m + 1))))
+		return (NULL);
+	i = -1;
+	start = 0;
+	count = 0;
+	while (str[++i])
+	{
+		if (str[i] == ' ')
+		{
+			key[count++] = get_short_string(str, start, i);
+			start = i + 1;
+		}
+	}
+	key[count++] = get_short_string(str, start, i);
+	key[count] = NULL;
+	return (key);
+}
 
 int			main(void)
 {
@@ -54,7 +94,7 @@ int			main(void)
 	char	**openkey1;
 	char	**openkey2;
 	char	**secret;
-	char	str[11] = {0, };
+	char	str[20001] = {0, };
 	int		*key;
 
 	//n 값 저장
@@ -66,29 +106,19 @@ int			main(void)
 		//단어의 갯수 저장
 		scanf("%d", &m);
 		getchar();
-		if (!(openkey1 = (char **)malloc(sizeof(char *) * (m + 1))))
-			return (0);
-		if (!(openkey2 = (char **)malloc(sizeof(char *) * (m + 1))))
-			return (0);
-		if (!(secret = (char **)malloc(sizeof(char *) * (m + 1))))
-			return (0);
 		j = -1;
 		while (++j < 3)
 		{
-			k = -1;
-			while (++k < m)
-			{
-				//문자열 입력
-				scanf("%s", str);
-				getchar();
-				//차례대로 오픈키1, 오픈키2, 암호문 저장
-				if (j == 0)
-					openkey1[k] = strdup(str);
-				else if (j == 1)
-					openkey2[k] = strdup(str);
-				else
-					secret[k] = strdup(str);
-			}
+			//문자열 입력
+			scanf("%[^\n]s", str);
+			getchar();
+			//차례대로 오픈키1, 오픈키2, 암호문 저장
+			if (j == 0)
+				openkey1 = get_string(str, m);
+			else if (j == 1)
+				openkey2 = get_string(str, m);
+			else
+				secret = get_string(str, m);
 		}
 		//키 값 구하기
 		key = get_key(openkey1, openkey2, m);
@@ -96,7 +126,7 @@ int			main(void)
 		get_plain_text(secret, key, m);
 		//초기화
 		j = -1;
-		while (++j < 11)
+		while (++j < 20001)
 			str[j] = 0;
 		//메모리 해제
 		j = -1;
