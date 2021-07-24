08a2c95dca0338cffc0deb709f16dbc49aaf01e9
윤현지 <hyoon@student.42seoul.kr>
2021-07-24 18:18:14 +0900

    final 9019 bfs

--- 9019.cpp
+++ 9019.cpp
@@ -15,9 +15,13 @@ int		main(){
 
 	cin >> n;
 	for (int i = 0; i < n; i++){
+		v.clear();
+		v.resize(10000);
+		while (!q.empty())
+			q.pop();
 		cin >> a >> b;
-		q.push(make_pair<int, string>(a, ""));
 		v[a] = true;
+		q.push(make_pair<int, string>(a, ""));
 		while (!q.empty()){
 			now = q.front().first;
 			way = q.front().second;
@@ -28,7 +32,7 @@ int		main(){
 			}
 			else{
 				next = (now * 2) % 10000;
-				if (!v[next]){
+				if (v[next] == false){
 					v[next] = true;
 					q.push(make_pair<int, string>(next, way + "D"));
 				}
@@ -36,18 +40,18 @@ int		main(){
 					next = 9999;
 				else
 					next = now - 1;
-				if (!v[next]){
+				if (v[next] == false){
 					v[next] = true;
 					q.push(make_pair<int, string>(next, way + "S"));
 				}
 				if (now != 0){
-					next = now % 1000 * 10 + now / 1000;
-					if (!v[next]){
+					next = (now % 1000) * 10 + now / 1000;
+					if (v[next] == false){
 						v[next] = true;
 						q.push(make_pair<int, string>(next, way + "L"));
 					}
-					next = now / 10 + now % 10 * 1000;
-					if (!v[next]){
+					next = now / 10 + (now % 10) * 1000;
+					if (v[next] == false){
 						v[next] = true;
 						q.push(make_pair<int, string>(next, way + "R"));
 					}
