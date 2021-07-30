7953ee06971defcbd955e11b07d12dcdbb6c1e6a
윤현지 <hyoon@student.42seoul.kr>
2021-07-31 00:43:57 +0900

    update 7576 2D tomato box

---
+++ 7576.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int		check_tomato(int num, int a, int b, int n, int m, vector<int> &v){
+	if (a > 0 && v[(a - 1) * n + b] == num)
+		return 1;
+	else if (a + 1 < m && v[(a + 1) * n + b] == num)
+		return 1;
+	else if (b > 0 && v[a * n + b - 1] == num)
+		return 1;
+	else if (b + 1 < n && v[a * n + b + 1] == num)
+		return 1;
+	return 0;
+}
+
+bool	check_map(int n, int m, vector<int> &v){
+	for (int i = 0; i < m; i++){
+		for (int j = 0; j < n; j++){
+			if (v[i * n + j] == 0)
+				return false;
+		}
+	}
+	return true;
+}
+
+int		cook_tomato(int num, int n, int m, vector<int> &v){
+	int		cnt = 0;
+
+	for (int i = 0; i < m; i++){
+		for (int j = 0; j < n; j++){
+			if (v[i * n + j] == 0 && check_tomato(num, i, j, n, m, v)){
+				cnt++;
+				v[i * n + j] = num + 1;
+			}
+		}
+	}
+	return cnt;
+}
+
+int		tomato(int n, int m, vector<int> &v){
+	int		i = 1;
+
+	while (cook_tomato(i, n, m, v))
+		i++;
+	if (check_map(n, m, v))
+		return i - 1;
+	return -1;
+}
+
+int		main(){
+	int	n, m;
+	vector<int>	v;
+
+	cin >> n >> m;
+	v.reserve(n * m);
+	for (int i = 0; i < m; i++){		//col
+		for (int j = 0; j < n; j++)		//row
+			cin >> v[i * n + j];
+	}
+	cout << tomato(n, m, v) << endl;
+	return 0;
+}
---
+++ 7576_2.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool	check_map(int n, int m, vector<int> &v){
+	for (int i = 0; i < m; i++){
+		for (int j = 0; j < n; j++){
+			if (v[i * n + j] == 0)
+				return false;
+		}
+	}
+	return true;
+}
+
+vector<int>	cook_tomato(int n, int m, vector<int> &v, vector<int> &apple){
+	int		s, a, b;
+	vector<int>	tmp;
+
+	s = apple.size();
+	for (int i = 0; i < s; i++){
+		a = apple[i] / n;
+		b = apple[i] % n;
+		if (a > 0 && v[(a - 1) * n + b] == 0){
+			v[(a - 1) * n + b] = 1;
+			tmp.push_back((a - 1) * n + b);
+		}
+		if (a + 1 < m && v[(a + 1) * n + b] == 0){
+			v[(a + 1) * n + b] = 1;
+			tmp.push_back((a + 1) * n + b);
+		}
+		if (b > 0 && v[a * n + b - 1] == 0){
+			v[a * n + b - 1] = 1;
+			tmp.push_back(a * n + b - 1);
+		}
+		if (b + 1 < n && v[a * n + b + 1] == 0){
+			v[a * n + b + 1] = 1;
+			tmp.push_back(a * n + b + 1);
+		}
+	}
+	return tmp;
+}
+
+int		tomato(int n, int m, vector<int> &v, vector<int> &apple){
+	int		i = 0;
+	vector<int>	tmp;
+
+	tmp = apple;
+	while (tmp.size() > 0){
+		tmp = cook_tomato(n, m, v, tmp);
+		i++;
+	}
+	if (check_map(n, m, v))
+		return i - 1;
+	return -1;
+}
+
+int		main(){
+	int	n, m;
+	vector<int>	v;
+	vector<int>	apple;
+
+	cin >> n >> m;
+	v.reserve(n * m);
+	for (int i = 0; i < m; i++){		//col
+		for (int j = 0; j < n; j++){		//row
+			cin >> v[i * n + j];
+			if (v[i * n + j] == 1)
+				apple.push_back(i * n + j);
+		}
+	}
+	cout << tomato(n, m, v, apple) << endl;
+	return 0;
+}
