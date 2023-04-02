0c308d4674a21854e84e2b858ad13645ed4d4fa8
Hyunji Yoon <kamg2218@hanmail.net>
2023-04-02 16:59:19 +0900

    [unrated] Title: 기사단원의 무기, Time: 1.12 ms, Memory: 35.5 MB -BaekjoonHub

--- 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.js
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.js
@@ -1,24 +1,19 @@
-function solution(number, limit, power) {
-    var answer = 0;
+const getDivisorCnt = (num) => {
+    let cnt = 0;
+    const last = Math.sqrt(num);
     
-    const getDivisorCnt = (num) => {
-        let cnt = 0;
-        const last = Math.sqrt(num);
-        
-        if (num === 1) return 1;
-        for (let i = 1; i <= last; i++) {
-            if (num % i === 0) cnt += 2;
-            if (i === last) cnt -= 1;
-        }
-        return cnt;
+    if (num === 1) return 1;
+    for (let i = 1; i <= last; i++) {
+        if (num % i === 0) cnt += 2;
+        if (i === last) cnt -= 1;
     }
-    
-    for (let i = 1; i <= number; i++) {
-        let cnt = getDivisorCnt(i);
-        if (cnt > limit) {
-            cnt = power;
-        }
-        answer += cnt;
-    }
-    return answer;
+    return cnt;
+}
+
+function solution(number, limit, power) {  
+    return new Array(number).fill(0).reduce((acc, _, idx) => {
+        const cnt = getDivisorCnt(idx + 1);
+        if (cnt > limit) return acc + power;
+        return acc + cnt;
+    }, 0);
 }
