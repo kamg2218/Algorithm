09d83c6022e97b6b526ace5da861efcb678b38cc
Hyunji Yoon <kamg2218@hanmail.net>
2023-04-04 21:22:49 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.62 ms, Memory: 33.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.js
@@ -0,0 +1,24 @@
+function solution(new_id) {
+    var answer = '';
+    const regExp = '[a-z 0-9 \_.-]';
+    
+    let result = new_id.toLowerCase().split('').filter(s => s.match(regExp)).join('');
+    
+    const replace = (before, after) => {
+        let tmp = result.replaceAll(before, after);
+        while (tmp !== result) {
+            result = tmp;
+            tmp = tmp.replaceAll(before, after);
+        }
+    }
+    
+    replace('..', '.');
+    if (result[0] === '.') result = result.slice(1);
+    if (result.at(-1) === '.') result = result.slice(0, -1);
+    if (result.length === 0) result = 'a';
+    if (result.length > 15) result = result.slice(0, 15);
+    if (result[0] === '.') result = result.slice(1);
+    if (result.at(-1) === '.') result = result.slice(0, -1);
+    if (result.length < 3) result += String(result.at(-1)).repeat(3 - result.length);
+    return result;
+}
