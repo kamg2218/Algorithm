79aaa53c08ca4ca14c62b97d1f0e34bf63be0964
Hyunji Yoon <kamg2218@hanmail.net>
2023-05-14 14:01:41 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.js
@@ -0,0 +1,13 @@
+function solution(ingredient) {
+    var answer = 0;
+    
+    for (let i = 0; i < ingredient.length - 3; i++) {
+       if (ingredient[i] === 1 && ingredient[i + 1] === 2
+          && ingredient[i + 2] === 3 && ingredient[i + 3] === 1) {
+            answer++;
+            ingredient.splice(i, 4);
+            i -= 4;
+        }
+    }
+    return answer;
+}
