1f5906497ba6a5d2440b31075e1f376a11cacb96
Hyunji Yoon <kamg2218@hanmail.net>
2023-07-06 23:40:05 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,10 @@
+function solution(num) {
+    var answer = 0;
+    while (num !== 1) {
+        if (answer >= 500) return -1;
+        if (num % 2) num = num * 3 + 1;
+        else num /= 2;
+        answer++;
+    }
+    return answer;
+}
