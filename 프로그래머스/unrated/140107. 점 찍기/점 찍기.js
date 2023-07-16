0d14ae852c4dd24bcc72051113d0a88cc038b2e1
Hyunji Yoon <kamg2218@hanmail.net>
2023-07-17 01:56:39 +0900

    [unrated] Title: 점 찍기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/140107. 점 찍기/점 찍기.js
+++ 프로그래머스/unrated/140107. 점 찍기/점 찍기.js
@@ -1,5 +1,4 @@
 function solution(k, d) {
-    const num = Math.floor(d / k);
     let answer = 0;
     for (let i = 0; i <= d; i += k) {
         answer += Math.floor(Math.sqrt(d * d - i * i) / k) + 1;
