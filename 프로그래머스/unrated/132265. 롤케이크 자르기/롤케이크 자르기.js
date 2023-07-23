cefbab7043c94280e1cdea5a54e9551ec93634ee
Hyunji Yoon <kamg2218@hanmail.net>
2023-07-24 00:30:21 +0900

    [unrated] Title: 롤케이크 자르기, Time: 120.39 ms, Memory: 71.4 MB -BaekjoonHub

--- 프로그래머스/unrated/132265. 롤케이크 자르기/롤케이크 자르기.js
+++ 프로그래머스/unrated/132265. 롤케이크 자르기/롤케이크 자르기.js
@@ -2,6 +2,7 @@ function solution(topping) {
     var answer = 0;
     const right = new Map();
     const left = new Set();
+    
     topping.forEach(top => {
         if (right.has(top)) right.set(top, right.get(top) + 1);
         else right.set(top, 1);
@@ -14,10 +15,3 @@ function solution(topping) {
     })
     return answer;
 }
-
-    // topping.forEach((top, idx) => {
-    //     const first = new Set(topping.slice(0, idx + 1)).size;
-    //     const second = new Set(topping.slice(idx + 1)).size;
-    //     if (first === second) answer++;
-    // })
-    
