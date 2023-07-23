2ae10b9160d11ae1199eba0f91ef4a25b9328a74
Hyunji Yoon <kamg2218@hanmail.net>
2023-07-23 22:41:13 +0900

    [level 2] Title: 줄 서는 방법, Time: 0.15 ms, Memory: 32.9 MB -BaekjoonHub

--- 프로그래머스/lv2/12936. 줄 서는 방법/README.md
+++ 프로그래머스/lv2/12936. 줄 서는 방법/README.md
@@ -1,10 +1,10 @@
 # [level 2] 줄 서는 방법 - 12936 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12936#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12936) 
 
 ### 성능 요약
 
-메모리: 33.1 MB, 시간: 0.21 ms
+메모리: 32.9 MB, 시간: 0.15 ms
 
 ### 구분
 
--- 프로그래머스/lv2/12936. 줄 서는 방법/줄 서는 방법.js
+++ 프로그래머스/lv2/12936. 줄 서는 방법/줄 서는 방법.js
@@ -1,25 +1,35 @@
 function solution(n, k) {
     var answer = [];
     const result = Array(n).fill(0).map((_, idx) => idx + 1);
+    let factorial = result.reduce((acc, cur) => acc * cur, 1);
+    let num = n;
     
-    const getFactorial = (num) => {
-        if (num <= 1) return 1;
-        else if (num === 2) return 2;
-        return num * getFactorial(num - 1);
+    while (answer.length < n) {
+        factorial /= num--;
+        const tmp = result.splice(Math.floor((k - 1) / factorial), 1);
+        answer.push(...tmp);
+        k %= factorial;
     }
+    return answer;
+//     const getFactorial = (num) => {
+//         if (num <= 1) return 1;
+//         else if (num === 2) return 2;
+//         return num * getFactorial(num - 1);
+//     }
     
-    k -= 1;
-    for (let i = n - 1; i > -1; i--) {
-        const factorial = getFactorial(i);
-        if (k >= factorial) {
-            const quote = Math.floor(k / factorial);
-            answer.push(result[quote]);
-            result.splice(quote, 1);
-            k -= quote * factorial;
-        } else {
-            answer.push(result[0]);
-            result.shift();
-        }
-    }
-    return answer.concat(result);
+//     k -= 1;
+//     for (let i = n - 1; i > -1; i--) {
+//         const factorial = getFactorial(i);
+//         if (k >= factorial) {
+//             const quote = Math.floor(k / factorial);
+//             answer.push(result[quote]);
+//             result.splice(quote, 1);
+//             k -= quote * factorial;
+//         } else {
+//             answer.push(result[0]);
+//             result.shift();
+//         }
+//     }
+//     return answer.concat(result);
 }
+
