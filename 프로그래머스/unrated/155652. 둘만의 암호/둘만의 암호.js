4e9b9c75c8ca6f5ac2adff28a39ec0b271be046e
Hyunji Yoon <kamg2218@hanmail.net>
2023-08-06 21:51:20 +0900

    [unrated] Title: 둘만의 암호, Time: 19.35 ms, Memory: 34.9 MB -BaekjoonHub

--- 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.js
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.js
@@ -1,16 +1,40 @@
-function solution(s, skip, index) {
-    const str = 'abcdefghijklmnopqrstuvwxyz';
-    const splitedStr = str.split('');
+// function solution(s, skip, index) {
+//     const str = 'abcdefghijklmnopqrstuvwxyz';
+//     const splitedStr = str.split('');
 
-    return s.split('').map(c => {
-        const idx = str.indexOf(c);
-        let num = idx;
-        for (let i = 0; i < index; i++) {
-            num++;
-            if (skip.includes(splitedStr[num % str.length])) {
-                i--;
-            }
-        }
-        return splitedStr[num % str.length];
-    }).join('');    
+//     return s.split('').map(c => {
+//         const idx = str.indexOf(c);
+//         let num = idx;
+//         for (let i = 0; i < index; i++) {
+//             num++;
+//             if (skip.includes(splitedStr[num % str.length])) {
+//                 i--;
+//             }
+//         }
+//         return splitedStr[num % str.length];
+//     }).join('');    
+// }
+
+function solution(s, skip, index) {
+
+const ALPAHBET = 'abcdefghijklmnopqrstuvwxyz'
+
+let result = []
+for(let i = 0; s.length-1 >= i; i++){
+    console.log(i, s[i]);
+    const answer = [];
+    const findIndex = ALPAHBET.indexOf(s[i]);
+    const num = index;
+    for(let j = findIndex + 1; ALPAHBET.length>=j; j++){
+        console.log(j, ALPAHBET[j]);
+        
+        if(j === 26) j = 0;
+        if(num === answer.length) break;
+        if(skip.includes(ALPAHBET[j])) continue;
+        answer.push(ALPAHBET[j])
+    }
+    console.log(answer)
+    result.push(answer.pop())
+}
+return result.join("");
 }
