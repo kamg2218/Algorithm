d797f066ed3110e1478692c17c7d08a5d0efdc60
Hyunji Yoon <kamg2218@hanmail.net>
2023-08-25 01:06:29 +0900

    Time: 73 ms (54.43%), Space: 45.8 MB (44.66%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.js
+++ 0036-valid-sudoku/0036-valid-sudoku.js
@@ -5,20 +5,17 @@
 var isValidSudoku = function(board) {
     //row    
     for (let i = 0; i < 9; i++) {
-        const nums = new Set();
+        const row = new Set();
+        const col = new Set();
         for (let j = 0; j < 9; j++) {
-            if (isNaN(board[i][j])) continue ;
-            if (nums.has(board[i][j])) return false;
-            nums.add(board[i][j]);
-        }
-    }
-    //col
-    for (let i = 0; i < 9; i++) {
-        const nums = new Set();
-        for (let j = 0; j < 9; j++) {
-            if (isNaN(board[j][i])) continue ;
-            if (nums.has(board[j][i])) return false;
-            nums.add(board[j][i]);
+            if (!isNaN(board[i][j])) {
+                if (row.has(board[i][j])) return false;
+                row.add(board[i][j]);
+            }
+            if (!isNaN(board[j][i])) {
+                if (col.has(board[j][i])) return false;
+                col.add(board[j][i]);
+            }
         }
     }
     //box
