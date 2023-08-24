7fc5a434f645e06d789ce83cce9320e7497af2c6
Hyunji Yoon <kamg2218@hanmail.net>
2023-08-25 00:24:06 +0900

    Time: 8233 ms (6.44%), Space: 53 MB (86.73%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -3,14 +3,8 @@
  * @return {number[]}
  */
 var productExceptSelf = function(nums) {
-//     const mul = nums.reduce((acc, cur) => acc * cur, 1);
-    
-//     return nums.map(num => mul / num);
-    
-    return nums.map((num, idx) => {
-        return nums.reduce((acc, cur, i) => {
-            if (i === idx) return acc;
-            return acc * cur;
-        }, 1);
-    });
+    return nums.map((num, idx) => nums.reduce((acc, cur, i) => {
+        if (i === idx) return acc;
+        return acc * cur;
+    }, 1));
 };
