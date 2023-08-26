c62b53a0c6b65b2727a7f40059ccc61a7df99446
Hyunji Yoon <kamg2218@hanmail.net>
2023-08-27 00:48:37 +0900

    Time: 114 ms (63.56%), Space: 65.5 MB (23.54%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
@@ -3,14 +3,11 @@
  * @return {number}
  */
 var longestConsecutive = function(nums) {
-    const numbers = [...new Set(nums)];
-    
-    numbers.sort((a, b) => a - b);
-    
     let max = 0;
     let cnt = 0;
     let last = Infinity;
-    numbers.forEach(num => {
+
+    [...new Set(nums)].sort((a, b) => a - b).forEach(num => {
         if (num - last !== 1) {
             if (cnt > max) max = cnt;
             cnt = 0;
