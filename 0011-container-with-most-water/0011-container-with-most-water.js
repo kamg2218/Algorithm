d32f4bf5be0ce22b95e0513b314538505076476d
Hyunji Yoon <kamg2218@hanmail.net>
2023-09-03 16:26:45 +0900

    Time: 55 ms (96.43%), Space: 49.4 MB (71.21%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.js
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -4,13 +4,7 @@
  */
 var maxArea = function(height) {
     let max = 0;
-    // for (let i = 0; i < height.length; i++) {
-    //     for (let j = i + 1; j < height.length; j++) {
-    //         const num = Math.min(height[i], height[j]) * (j - i);
-    //         max = Math.max(max, num);
-    //     }
-    // }
-    
+
     let left = 0;
     let right = height.length - 1;
     while (left < right) {
@@ -20,6 +14,5 @@ var maxArea = function(height) {
         if (height[left] <= height[right]) left++;
         else right--;
     }
-    
     return max;
 };
