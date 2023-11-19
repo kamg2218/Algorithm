d41e4bc606e53bfb65ad9d26e84f8ad74cfe48f7
Hyunji Yoon <kamg2218@hanmail.net>
2023-11-19 22:40:33 +0900

    Time: 64 ms (27.91%), Space: 43.9 MB (66.43%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
@@ -15,15 +15,19 @@ var mergeTwoLists = function(list1, list2) {
     let dir = null;
     
     while (list1 && list2) {
-        const tmp = new ListNode(Math.min(list1.val, list2.val));
+        const tmp = new ListNode();
+        
+        if (list1.val <= list2.val) {
+            tmp.val = list1.val;
+            list1 = list1.next;
+        } else {
+            tmp.val = list2.val;
+            list2 = list2.next;
+        }
         
         if (!answer) answer = tmp;
         else dir.next = tmp;
-
-        dir = tmp;
-        
-        if (list1.val === tmp.val) list1 = list1.next;
-        else list2 = list2.next;
+        dir = tmp;        
     }
     if (!dir) return list1 ? list1 : list2;
     if (list1) dir.next = list1;
