80a847ea13ac7f75aa665c0a91a577d50e2df607
Hyunji Yoon <kamg2218@hanmail.net>
2023-12-10 14:43:53 +0900

    Time: 70 ms (78.74%), Space: 63.1 MB (9.77%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findDuplicate = function(nums) {
+    const map = new Map();
+    
+    for (let i = 0; i < nums.length; i++) {
+        if (map.has(nums[i])) return nums[i];
+        else map.set(nums[i], nums[i]);
+    }
+    return -1;
+};
